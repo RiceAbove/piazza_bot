440159c1909a22a24427a833541b9d8f5a206a5a
RiceAbove <evangelistagico@gmail.com>
2020-06-13 16:30:53 -0700

    working on parsing answers

--- test.py
+++ test.py
@@ -46,15 +46,52 @@ def piazza_parse(pi_url):
     temp += question_text + '\n\n'
 
     answers = post["children"]
-    print()
-
     temp += "__**ANSWERS**__\n"
     #TODO concatenate all answers? or just one
     #temp += answers
 
     return temp
 
-if __name__ == "__main__":
-    main()
+def sandbox():
+    p = Piazza()
+    p.user_login(email=os.environ['EMAIL'], password=os.environ['PASSWORD'])
+
+    piazza_url = urlparse(sys.argv[1])
+
+    class_id = piazza_url.path.split('/')[2]
+    post_num = piazza_url.query.split('=')[1]
+    
+    # Returns a class network
+    class_net = p.network(class_id)
+
+    post = class_net.get_post(post_num)
+
+    answers = post["children"]
+    s_answer_json = None
+    i_answer_json = None
+    print("ANSWERS")
+    for answer in answers:
+        pprint(answer)
+        if answer['type'] == 's_answer':
+            s_answer_json = answer
+        elif answer['type'] == 'i_answer':
+            i_answer_json = answer
+        print()
+    print()
+
+    if s_answer_json is not None:
+        print("STUDENT ANSWER")
+        s_answer = s_answer_json['history'][0]
+        pprint(s_answer)
+    print()
+
+    if i_answer_json is not None:
+        print("INSTRUCTOR ANSWER")
+        i_answer = i_answer_json['history']
+        pprint(i_answer)
+    print()
+
+# When you run this file, it will run the sandbox function for testing
+sandbox()
 
     
